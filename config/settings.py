a421aa0c0203ad64e0677e0c520beb4e1e21ff83
lokman hakim <lokmanhakimshawon3@gmail.com>
2026-07-12 16:48:49 +0600

    add prostgresql

--- config/settings.py
+++ config/settings.py
@@ -92,8 +92,12 @@ WSGI_APPLICATION = 'config.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'oil_api',
+        'USER':'postgres',
+        'PASSWORD':'12345678',
+        'HOST':'localhost',
+        'POST':'5432',
     }
 }
 
